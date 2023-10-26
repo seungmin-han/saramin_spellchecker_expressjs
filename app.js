61b57b8aafc2039713b3f37187928c1bd44496bd
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-10-26 16:36:03 +0900

    Update: 맞춤법 검사 파싱 처리 추가

--- app.js
+++ app.js
@@ -5,11 +5,19 @@ const port = 3000
 
 app.get('/', (req, res) => {
     const content = req.query.content;
-    console.log(req.query.content);
     axios.get('https://www.saramin.co.kr/zf_user/tools/spell-check', { params: { content } }).then(r => {
-        res.send(r.data);
+        const { original_text, word_list, result_cnt } = r.data;
+        let editedText = original_text;
+        word_list.map(v => {
+            editedText = editedText.replaceAll(v.errorWord, v.candWordList);
+        })
+
+        let wrong_list = word_list.map(v => {
+            return { errorWord: v.errorWord, editedWord: v.candWordList };
+        })
+
+        res.send({original_text, editedText, wrong_list, wrong_cnt: result_cnt});
     }).catch(err => {
-        console.log(err);
         res.send(err);
   })
 })
