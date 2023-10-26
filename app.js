8ba4ad23b382e379d0a907117ef25ffadd2ea9c0
SEUNGMIN HAN <seungmin_han@kakao.com>
2023-10-26 16:16:38 +0900

    Update: 사람인 스펠체크 API 확인

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-
 const express = require('express')
+const axios = require('axios')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    const content = req.query.content;
+    console.log(req.query.content);
+    axios.get('https://www.saramin.co.kr/zf_user/tools/spell-check', { params: { content } }).then(r => {
+        res.send(r.data);
+    }).catch(err => {
+        console.log(err);
+        res.send(err);
+  })
 })
 
 app.listen(port, () => {
